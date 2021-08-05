c5c81a745f8ba654ce54dbfb7b6293ca6f2f2e0c
Nadya Karaban <nadya@gini.net>
2021-08-05 11:26:59 +0200

    Review notes: set JPEG format in  AVCapturePhotoSettings (PIA-1642)

--- GiniCapture/Classes/Core/Screens/Camera/Camera.swift
+++ GiniCapture/Classes/Core/Screens/Camera/Camera.swift
@@ -186,8 +186,15 @@ final class Camera: NSObject, CameraProtocol {
 fileprivate extension Camera {
     
     var captureSettings: AVCapturePhotoSettings {
-        let captureSettings = AVCapturePhotoSettings()
-        
+        var captureSettings: AVCapturePhotoSettings
+        if #available(iOS 11.0, *) {
+            captureSettings = AVCapturePhotoSettings(rawPixelFormatType: 0,
+                                                         rawFileType: nil,
+                                                         processedFormat: nil,
+                                                         processedFileType: AVFileType.jpg)
+        } else {
+            captureSettings = AVCapturePhotoSettings()
+        }
         guard let device = self.videoDeviceInput?.device else { return captureSettings }
         
         #if !targetEnvironment(simulator)
