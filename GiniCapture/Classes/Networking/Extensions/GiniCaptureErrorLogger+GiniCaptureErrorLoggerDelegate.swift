a43d3bb1c6affb263639c2f2cc02c43f72a3860f
Nadya Karaban <nadya@gini.net>
2021-08-24 12:51:19 +0200

    fixed : error: type 'GiniCaptureErrorLogger' does not conform to protocol 'GiniCaptureErrorLoggerDelegate' (PIA-1645)

--- GiniCapture/Classes/Networking/Extensions/GiniCaptureErrorLogger+GiniCaptureErrorLoggerDelegate.swift
+++ GiniCapture/Classes/Networking/Extensions/GiniCaptureErrorLogger+GiniCaptureErrorLoggerDelegate.swift
@@ -8,12 +8,5 @@
 import Foundation
 
 extension GiniCaptureErrorLogger {
-    public func postGiniErrorLog(error: ErrorLog, apiLibVersion: String? = "") {
-        if isGiniLoggingOn {
-            print("GiniScreenAPICoordinator : Error logged to Gini: \(error)")
-        }
-        if let customErrorLogger = customErrorLogger {
-            customErrorLogger.postGiniErrorLog(error: error, apiLibVersion: error.apiLibVerion())
-        }
-    }
+   // error.apiLibVerion())
 }
