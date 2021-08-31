2548cc5b8355be206214640e385fd6e5095c1696
Nadya Karaban <nadya@gini.net>
2021-08-31 17:04:53 +0200

    iOS 15: Example swift: ScreenAPI: fixed Navigation bar disappearance on ResultsViewController (PIA-1652) ComponentAPI: Fixed Navigation bar style (PIA-1652) ComponentAPI: Fixed Tab bar style (PIA-1652)

--- Example/Example Swift/ComponentAPICoordinator.swift
+++ Example/Example Swift/ComponentAPICoordinator.swift
@@ -41,18 +41,23 @@ final class ComponentAPICoordinator: NSObject, Coordinator {
         navBarViewController.navigationBar.barTintColor = self.giniColor
         navBarViewController.navigationBar.tintColor = .white
         navBarViewController.view.backgroundColor = .black
-        
+        navBarViewController.applyStyle(withConfiguration: giniConfiguration)
         return navBarViewController
     }()
     
     fileprivate lazy var componentAPITabBarController: UITabBarController = {
         let tabBarViewController = UITabBarController()
-        tabBarViewController.tabBar.barTintColor = self.giniColor
-        tabBarViewController.tabBar.tintColor = .white
-        tabBarViewController.view.backgroundColor = .black
-        
+        if #available(iOS 15.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = self.giniColor
+            tabBarViewController.tabBar.standardAppearance = appearance
+            tabBarViewController.tabBar.scrollEdgeAppearance = tabBarViewController.tabBar.standardAppearance
+        } else {
+            tabBarViewController.tabBar.barTintColor = self.giniColor
+        }
+
         tabBarViewController.tabBar.unselectedItemTintColor = UIColor.white.withAlphaComponent(0.6)
-        
         return tabBarViewController
     }()
     
@@ -186,7 +191,8 @@ extension ComponentAPICoordinator {
         resultsScreen = storyboard.instantiateViewController(withIdentifier: "resultScreen")
             as? ResultTableViewController
         resultsScreen?.result = extractions
-        
+        navigationController.applyStyle(withConfiguration: giniConfiguration)
+
         if navigationController.viewControllers.first is AnalysisViewController {
             resultsScreen!.navigationItem
                 .rightBarButtonItem = UIBarButtonItem(title: NSLocalizedString("close",
@@ -213,7 +219,6 @@ extension ComponentAPICoordinator {
             genericNoResults!.delegate = self
             vc = genericNoResults!
         }
-        
         push(viewController: vc, removing: [reviewScreen, analysisScreen])
         
     }
